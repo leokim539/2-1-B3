2f5c87bb9f50effcbd0db468dd9763edd6f8efc5
Sunu0727 <168548439+Sunu0727@users.noreply.github.com>
2024-05-06 12:27:56 +0900

    JumpPad 점프대 완성
    위 2개의 스크립트를 Assets에 있는 scripts에 넣으시면 됩니다.
    그 후 플레이어에게 'Player'태그를 부착해주시고, JumpPad 오브젝트에 JumpPad스크립트를 부착해주시면 됩니다.

---
+++ JumpPad.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce = 10f; // 점프대의 힘
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 플레이어가 점프대와 충돌했는지 확인
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            if (playerRigidbody != null)
+            {
+                // 플레이어를 위로 튕겨내는 힘 적용
+                playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
