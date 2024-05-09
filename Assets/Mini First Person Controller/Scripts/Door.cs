2b071d98cddffdb347308df77633ec6ebcf464ac
namjiseong12 <namjiseong12@naver.com>
2024-05-09 21:49:54 +0900

    수정 내용 많음
    1. 플레이어의 콜라이더를 캡슐에서 육면체로 변경함 > 낮은 발판에 비비면서 점플 할 때 튕겨 오르는 현상 해결
    
    2. 움직이는 발판 위에서 점프 기능 구현 (정상적인 방안이 아니라고 생각함... 일단 임시로 해결은 해 뒀는데 점프가 조금 어색한 느낌이 있음)
    
    3. 문 임시로 만듦
    
    수정 내용 많아서 전체 폴더로 올립니다. 다음 각자 부분들 만들기 전에 이 파일 받은 뒤 만들어주세요.

---
+++ Assets/Mini First Person Controller/Scripts/Door.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Vector3 initialPosition;
+    public Vector3 targetPosition;
+    private bool isColliding = false; 
+    // 문을 여는  메서드
+    public void OpenDoor(float speed)
+    {
+        isColliding = true;
+        // Coroutine을 시작하여 문을 지정 위치로 이동시킨다
+        StartCoroutine(MoveDoor(targetPosition, speed));
+    }
+
+    // 문을 닫기 위한 메서드
+    public void CloseDoor(float speed)
+    {
+        isColliding = false;
+        // Coroutine을 시작하여 문을 초기 위치로 이동시킨ㄷ
+        StartCoroutine(CloseDoorCoroutine(initialPosition, speed));
+    }
+
+    private IEnumerator MoveDoor(Vector3 newPosition, float speed)
+    {
+        while (transform.position != newPosition)
+        {
+            if (!isColliding) // 만약 isCollding 이 false이라면 코루틴 정지 
+        {
+            yield break;
+        }
+            transform.position = Vector3.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private IEnumerator CloseDoorCoroutine(Vector3 initialPosition, float speed)
+    {
+        while (transform.position != initialPosition)
+        {
+            if (isColliding) // 만약 isCollding 이 true이라면 코루틴 정지 
+        {
+            yield break;
+        }
+            transform.position = Vector3.MoveTowards(transform.position, initialPosition, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
