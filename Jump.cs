ac5888f61d0333d226ad6a02f910d0f484f85a33
leokim539 <167206735+leokim539@users.noreply.github.com>
2024-05-13 22:05:37 +0900

    이단점프 아이템 적용
    점프 코드 그대로 갈아 끼우기 만든 아이템에 item 태그 붙이기

---
+++ Jump.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float jumpStrength = 2;
+    public event System.Action Jumped;
+    public int jumpPossible; 
+    int jumpCount;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        jumpCount = jumpPossible;
+    }
+
+    void Update()
+    {
+        Jump1();
+    }
+    void Jump1()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
+            Jumped?.Invoke();
+            jumpCount--;
+            
+
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "item")
+        {
+            jumpCount++;
+            Destroy(other.gameObject);
+        }
+        if (jumpCount == 0)
+        {
+            jumpCount++;
+        }
+    }
+}
+
